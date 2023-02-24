56b2db4702676818bead4371e7f8867204fccc69
Rowedahelicon <matthewr01@gmail.com>
2023-02-24 08:29:05 -0500

    Initial commit

---
+++ public_html/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+        <title>Hi, hello!</title>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.tailwindcss.com/3.0.23"></script>
+    <body>
+
+    <div id="container" class="container md:mx-auto max-w-screen-lg md:my-4 rounded-lg border-solid border-0 md:border-8 border-black/70">
+
+        <Header>
+
+        </Header>
+        <nav>
+
+        </nav>
+        <main>
+            
+        </main>
+
+    </div>
+
+    </body>
+</html> 
