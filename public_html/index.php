ba27818b1ec5c64f0d2f099b14766953bac85b79
Rowedahelicon <hello@rowedahelicon.com>
2023-03-05 02:55:03 -0500

    Added minified assets

--- public_html/index.php
+++ public_html/index.php
@@ -8,11 +8,11 @@
         <title><?php echo $GLOBALS['config']['title']; ?></title>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="assets/css/main.css">
-        <link rel="stylesheet" href="assets/css/tailwind.css">
-        <link href="assets/fontawesome/fontawesome.css" rel="stylesheet">
-        <link href="assets/fontawesome/brands.css" rel="stylesheet">
-        <link href="assets/fontawesome/solid.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/main.min.css">
+        <link rel="stylesheet" href="assets/css/tailwind.min.css">
+        <link href="assets/fontawesome/fontawesome.min.css" rel="stylesheet">
+        <link href="assets/fontawesome/brands.min.css" rel="stylesheet">
+        <link href="assets/fontawesome/solid.min.css" rel="stylesheet">
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
