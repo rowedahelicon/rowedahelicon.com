25687d018540df6ee82ee3ca655c0d442590994b
Rowedahelicon <hello@rowedahelicon.com>
2023-03-04 21:15:17 -0500

    Setup

--- public_html/index.php
+++ public_html/index.php
@@ -5,18 +5,35 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://cdn.tailwindcss.com/3.0.23"></script>
+        <link rel="stylesheet" href="assets/css/main.css">
     <body>
-
-    <div id="container" class="container md:mx-auto max-w-screen-lg md:my-4 rounded-lg border-solid border-0 md:border-8 border-black/70">
-
+    <div id="stars_1"></div>
+    <div id="stars_2"></div>
+    <div id="stars_3"></div>
+    <div id="container" class="container md:mx-auto max-w-screen-lg md:my-4 rounded-lg stripes-1 p-2 border-solid border-0 md:border-8 border-black/70">
+        <nav aria-label="Social Media Links" class="rounded-t-lg">
+f
+        </nav>
         <Header>
-
+            <img src="https://www.rowdys.art/assets/images/wb_banner.webp?v=1">
         </Header>
         <nav>
-
+        <ol class="list-reset flex p-3 text-normal text-gray-200">
+                        <li><a href="/">Home</a></li>
+                                                    <li><span class="mx-2">/</span></li>
+                            <li>About</li>                                            </ol>
         </nav>
-        <main>
-            
+        <main class="p-2">
+            <section class="p-2">
+                <img src="https://www.rowdys.art/assets/images/stargazer.png" alt="Rowdy" class="border-solid border-0 md:border-8 border-black/70">
+
+                <div class="text-center text-white">
+
+                    <h1 class="text-3xl">Rowedahelicon</h1>
+                </div>
+
+                <hr class="light" >
+            </section>
         </main>
 
     </div>
